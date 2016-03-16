974e2a35e99cd39ca95c6363a4e801a8b1c477cd
Brad Hanks <hanks.brad@gmail.com>
2016-03-16 16:37:49 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+var fresh2zip = require('./fresh2zip.js');
+
+if (false) {
+  fresh2zip.fresh2zip();
+} else {
+  fresh2zip.fresh2zip(
+    '',                     // email
+    '',                                                     // password
+    '',                             // freshbooks token
+    '' // freshbooks api subdomain
+    );
+}
